a5e5bc3f909f3aa0fa19c65fa5652929edaccce5
Jenna Tan <9787333+jenna215@users.noreply.github.com>
2023-01-11 23:36:54 -0500

    changed json to txt

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from bookSearch import *
 import sys
 
-
 def main():
 # CLI UI with choices for using Google Books API
-# Currently, only 1 and 3 work...
     choice = 0
     while choice != 3:
         print("***Books Manager***")
@@ -16,12 +14,12 @@ def main():
 # to return the first result of your book's title, author, and publisher. 
         if choice == "1":
             print("Looking up a book...")
-            bookManager = Books("./Data/bookData.json")
+            bookManager = Books("./Data/bookData.txt")
             bookManager.searchBookinAPI()
-# Having an issue here with connecting. Looking forward to feedback and suggestions.
+
         elif choice == "2":
             print("Displaying all books...")
-            bookManager = Books("./Data/bookData.json")
+            bookManager = Books("./Data/bookData.txt")
             bookManager.showBooks()
 # Uses sys module to quit program after finished viewing search results. 
         else:
